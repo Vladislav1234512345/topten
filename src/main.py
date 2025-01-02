b26d7ddd5bb5e941a644131824f8dea1101be3aa
Vladislav1234512345 <tolerantniy2104@gmail.com>
2025-01-03 00:29:31 +0400

    feat: alembic/, alembic.ini

--- src/main.py
+++ src/main.py
@@ -12,6 +12,10 @@ import uvicorn
 from v1 import router as v1_router
 from database import create_db_and_tables
 from config import web_settings
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+
 
 
 @asynccontextmanager
@@ -24,6 +28,10 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=v1_router)
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"],allow_credentials=True)
+app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 
 
 if __name__ == '__main__':
